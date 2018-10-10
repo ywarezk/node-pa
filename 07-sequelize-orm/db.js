6505fd6adb3e9f60be53273341275649d9ac2da3
Yariv Katz <ywarezk@gmail.com>
2018-10-10 17:11:15 +0300

    added docker run configu

--- 07-sequelize-orm/db.js
+++ 07-sequelize-orm/db.js
@@ -12,3 +12,9 @@ sequelize.import('./models/user');
 sequelize.import('./models/task');
 
 module.exports = sequelize;
+
+// to run through docker
+
+// docker run --name postgresql4 --restart always --env 'PG_TRUST_LOCALNET=true' -p 5432:5432 sameersbn/postgresql:10
+// createdb -h 0.0.0.0 -U postgres todo
+// psql -h 0.0.0.0 -U postgres todo
